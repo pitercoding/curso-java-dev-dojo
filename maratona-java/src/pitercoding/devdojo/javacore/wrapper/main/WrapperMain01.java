24e007976403850c6aecc7a499a829ebaaece94f
pitercoding <piterg.bio@gmail.com>
2026-01-21 20:12:53 +0100

    feat: implementa classes wrapper e autoboxing em Java
    - uso das classes wrapper (Integer, Double, etc.)
    - autoboxing e unboxing
    - conversão entre tipos primitivos e objetos

---
+++ maratona-java/src/pitercoding/devdojo/javacore/wrapper/main/WrapperMain01.java
@@ -0,0 +1,34 @@
+package pitercoding.devdojo.javacore.wrapper.main;
+
+public class WrapperMain01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'P';
+        boolean booleanP = false;
+
+        Byte byteW = 1; // boxing
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'P';
+        Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+    }
+}

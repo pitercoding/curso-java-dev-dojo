da840a7a7629dc0a6326fc7fd569a0d9c3868792
pitercoding <piterg.bio@gmail.com>
2026-01-27 12:27:15 +0100

    feat: adiciona exemplos de internacionalização com ResourceBundle

---
+++ maratona-java/src/pitercoding/devdojo/javacore/resourcebundle/main/ResourceBundleMain01.java
@@ -0,0 +1,17 @@
+package pitercoding.devdojo.javacore.resourcebundle.main;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleMain01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", new Locale("en", "US"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        ResourceBundle bundle2 = ResourceBundle.getBundle("messages", new Locale("pt", "BR"));
+        System.out.println(bundle2.getString("hello"));
+        System.out.println(bundle2.getString("good.morning"));
+    }
+}

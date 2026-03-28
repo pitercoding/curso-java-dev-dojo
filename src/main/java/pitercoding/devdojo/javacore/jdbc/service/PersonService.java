97684f06bf0c62d70e1571df181cc0199ead235d
pitercoding <piterg.bio@gmail.com>
2026-03-28 10:14:44 +0100

    test(junit): expande testes unitários e cobre novos cenários (aula 281)

--- src/main/java/pitercoding/devdojo/javacore/jdbc/service/PersonService.java
+++ src/main/java/pitercoding/devdojo/javacore/jdbc/service/PersonService.java
@@ -2,11 +2,17 @@ package pitercoding.devdojo.javacore.jdbc.service;
 
 import pitercoding.devdojo.javacore.jdbc.domain.Person;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PersonService {
     public boolean isAdult(Person person){
         Objects.requireNonNull(person, "Person can't be null");
         return person.getAge() >= 18;
+    }
+
+    public List<Person> filterRemovingNotAdults(List<Person> personList){
+        return personList.stream().filter(this::isAdult).collect(Collectors.toList());
     }
 }
--- src/test/java/pitercoding/devdojo/javacore/jdbc/service/PersonServiceTest.java
+++ src/test/java/pitercoding/devdojo/javacore/jdbc/service/PersonServiceTest.java
@@ -1,17 +1,49 @@
 package pitercoding.devdojo.javacore.jdbc.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pitercoding.devdojo.javacore.jdbc.domain.Person;
 
-class PersonServiceTest {
+import java.util.Arrays;
+import java.util.List;
 
+class PersonServiceTest {
+    private Person adult;
+    private Person notAdult;
+    private PersonService personService;
+    @BeforeEach
+    public void setUp() {
+        adult = new Person(18);
+        notAdult = new Person(15);
+        personService = new PersonService();
+    }
     @Test
     @DisplayName("A person should be not adult when age is lower than 18")
     void isAdult_ReturnFalse_WhenAgeIsLowerThan18() {
-        Person person = new Person(15);
-        PersonService personService = new PersonService();
-        Assertions.assertFalse(personService.isAdult(person));
+        Assertions.assertFalse(personService.isAdult(notAdult));
+    }
+
+    @Test
+    @DisplayName("A person should be adult when age is equal or greater than 18")
+    void isAdult_ReturnTrue_WhenAgeIsEqualOrGreaterThan18() {
+        Assertions.assertTrue(personService.isAdult(adult));
+    }
+
+    @Test
+    @DisplayName("Should throw NullPointerException with message when person is null")
+    void isAdult_ThrowNullPointerException_WhenPersonIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> personService.isAdult(null), "Person can't be null");
+    }
+
+    @Test
+    @DisplayName("Should return a list with adults only")
+    void filterRemovingNotAdults_ReturnListWithAdultOnly_WhenListOfPersonWithAdultIsPassed() {
+        Person person1 = new Person(17);
+        Person person2 = new Person(18);
+        Person person3 = new Person(21);
+        List<Person> personList = List.of(person1, person2, person3);
+        Assertions.assertEquals(2,personService.filterRemovingNotAdults(personList).size());
     }
 }
